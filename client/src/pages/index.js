77ad4b81a4d4310119db088892c69d40afcc4a46
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-11-21 14:41:17 +0100

    first new source commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,14 @@
+import Home from './Home'
+import ProductDetails from './ProductDetails'
+import Products from './Products'
+import Search from './Search'
+
+
+
+
+export {
+    Home,
+    ProductDetails,
+    Products,
+    Search
+}
