d219d541d3a12f5dc4431904792a5a7980ff2ae1
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-11-22 17:07:05 +0100

    first commit

--- client/src/components/ProductSlider.jsx
+++ client/src/components/ProductSlider.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+//import swiper
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-const ProductSlider = () => {
-  return <div>ProductSlider</div>;
+//import swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+
+import '../slider.css'
+
+//import required modules
+import { Pagination, Navigation } from 'swiper'
+import { Product } from './';
+
+
+
+const ProductSlider = ({data}) => {
+  
+  return (
+    <Swiper
+    modules={[Pagination, Navigation]}
+    loop={false}
+    navigation={true}
+    pagination={{
+      clickable: true
+    }}
+    className='productSlider mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px]'
+    breakpoints={{
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30 
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      }}
+      
+  }
+    >
+      <>
+      {
+        data?.map((product, i )=> (
+          <SwiperSlide key={i}>
+            <Product product={product} />
+          </SwiperSlide>
+        ))
+      }
+      </>
+    </Swiper>
+
+  )
 };
 
 export default ProductSlider;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
+import { ProductSlider } from '../components';
 
 const Home = () => {
-  return <div>Home</div>;
+  //get new products
+  const {data} = useFetch('/products?populate=*&filters[isNew]=true')
+  
+
+
+  return (
+    <div className='mb-18'>
+      <div className="container mx-auto">
+        <h2 className='h2 mb-6 text-center xl:text-left'>
+          Latest Products</h2>
+      </div>
+      <ProductSlider data={data} />
+    </div>
+  );
 };
 
 export default Home;
