25d1fb876d3fcc21769c333b943279ddfe98db08
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-11-22 15:29:29 +0100

    api commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { Footer, Header } from './components';
+import { Home, ProductDetails, Products, Search } from './pages';
+
+
+//layout
+
+const Layout = () => {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {path: '/', element: <Home />},
+      {path: '/products/:id', element: <Products />},
+      {path: '/product/:id', element: <ProductDetails />},
+      {path: '/search', element: <Search />},
+    ]
+  }
+])
 
 const App = () => {
   return <div>
-    App
+    <RouterProvider router={router} />
     </div>;
 };
 
